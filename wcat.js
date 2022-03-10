75d98af62eb17a9cb8f39d18a83cb1dfb1eef778
arrxy <justadepressedguy69@gmail.com>
2022-03-10 21:59:07 +0530

    2nd feature

--- wcat.js
+++ wcat.js
@@ -1,6 +1,5 @@
 // 1) node wcat.js filepath => displays the contents of a file in terminal
 // 2) node wcat.js -n filepath1 filepath2 filepath3
-//
 const fs = require("fs");
 let inputArr = process.argv.slice(2);
 
@@ -8,10 +7,14 @@ let filesArr = [];
 let optionsArr = [];
 for(let i = 0; i < inputArr.length; i++)
 {
-    // let firstChar = inputArr
-    filesArr.push(inputArr[i]);
+    let firstChar = inputArr[i].charAt(0);
+    if(firstChar == '-'){
+        optionsArr.push(inputArr[i]);
+    }else{
+        filesArr.push(inputArr[i]);
+    }
+    
 }
-// console.log("files to be read " + filesArr);
 
 // check if files are present
 
@@ -31,3 +34,27 @@ for(let i = 0; i < filesArr.length; i++){
     content += fileContent + "\n";
 }
 console.log(content);
+
+let contentArr = content.split("\n");
+console.table(contentArr);
+
+let is_S_present = optionsArr.includes("-s");
+let is_N_present = optionsArr.includes("-n");
+let is_B_present = optionsArr.includes("-b");
+if(is_S_present){
+    for(let i = 1; i < contentArr.length; i++){
+        if(contentArr[i] == "" && (contentArr[i - 1] == "" || contentArr[i - 1] == null)){
+            contentArr[i] = null;
+        }
+    }
+    let tempArr = [];
+    for(let i = 0; i < contentArr.length; i++){
+        if(contentArr[i] != null)
+        {
+            tempArr.push(contentArr[i]);
+        }
+    }
+    console.log(tempArr);
+}
+// if(contentArr[i] == "" && (contentArr[i - 1] == "" || content[i - 1] == null))
+// console.table(contentArr);
